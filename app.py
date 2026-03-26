57a5a37222f1a6ab3fb38bdbe5bce999b462d705
Minttu Gomez <minttu.gomez@helsinki.fi>
2026-03-26 12:35:27 +0200

    Add registration

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import sqlite3
 from flask import Flask
-from flask import render_template
+from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
+import db
 
 app = Flask(__name__)
 
@@ -10,6 +13,23 @@ def index():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/create_account", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "ERROR: passwords do not match"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "ERROR: username not available"
+
+    return "Account created"
 
 @app.route("/login")
 def login():
